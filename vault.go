41c214485ce756d6b349a3719020057b18dd7f53
daqingshu <1711953+daqingshu@user.noreply.gitee.com>
2022-01-13 09:41:12 +0800

    upgrade vault api and support kv2

--- vault.go
+++ vault.go
@@ -24,8 +24,15 @@ func getCredentialSetFromVault(host string) (map[string]string, error) {
 		return nil, errNoData
 	}
 
-	var out = map[string]string{}
-	for k, v := range secret.Data {
+	var (
+		out  = map[string]string{}
+		data map[string]interface{}
+		ok   bool
+	)
+	if data, ok = secret.Data["data"].(map[string]interface{}); !ok {
+		data = secret.Data
+	}
+	for k, v := range data {
 		vs, ok := v.(string)
 		if !ok {
 			return nil, errors.Errorf("Key %q contained invalid data type %T", k, v)
